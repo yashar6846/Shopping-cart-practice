bb532f0854ba96ff4a972afdb6e2c643f2b6fe41
yashar6846 <yasharnajafi6846@gmail.com>
2021-04-07 21:53:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css';
+import Cart from './Commonents/Cart'
+import Products from './Commonents/Products'
+import Menu from './Commonents/Menu'
+
+function App() {
+  return (
+    <div className="App">
+     <Cart />
+     <Products />
+     <Menu />
+
+    </div>
+  );
+}
+
+export default App;
